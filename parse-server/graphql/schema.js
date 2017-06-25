919a3321375a58cc40b639d1edc4d0d63db7cc36
Tybi <developer@dwastudio.fr>
2017-06-25 14:52:17 +0200

    add object types

--- parse-server/graphql/schema.js
+++ parse-server/graphql/schema.js
@@ -11,3 +11,43 @@ import {
 } from 'graphql';
 import Parse from 'parse/node';
 
+/********* Object Types *********/
+
+const userType = new GraphQLObjectType({
+  name: 'User',
+  description: 'A simple user',
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+      resolve: (obj) => obj.id
+    },
+    username: {
+      type: GraphQLString,
+      resolve: (obj) => obj.get('username')
+    },
+    sessionToken: {
+      type: GraphQLString,
+      resolve: (obj) => obj.getSessionToken()
+    }
+  })
+});
+
+const postType = new GraphQLObjectType({
+  name: 'Post',
+  description: 'A simple post message',
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+      resolve: (obj) => obj.id
+    },
+    message: {
+      type: GraphQLString,
+      resolve: (obj) => obj.get('message'),
+    },
+    author: {
+      type: userType,
+      resolve: (obj) => obj.get('author'),
+    }
+  })
+});
+
